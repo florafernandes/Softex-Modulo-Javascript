61c3562828849c5c13e3b0f894109aca38c93b04
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-09-18 11:31:57 -0300

    Calculadora JavaScript
    Escolha uma operação para fazer entre dois números e receba um erro se digitar um operador inexistente.

---
+++ CALCULADORA.js
@@ -0,0 +1,27 @@
+var value1
+var valor2
+var oper
+var readlineSync = require('readline-sync');
+operator = readlineSync.question("Insira o simbolo da operacao que deseja efetuar: (+), (-),(*) ou (/)? : \n");
+value1 = parseFloat(readlineSync.question("Insira o primeiro numero: \n"));
+value2 = parseFloat(readlineSync.question("Insira o segundo numero: \n"));
+
+function doOperation(operator, value1, value2) {
+    if (operator == "+") {
+        return value1 + value2;
+    } else if
+        (operator == "-") {
+        return value1 - value2;
+    } else if
+        (operator == "*") {
+        return value1 * value2;
+    } else if
+        (operator == "/") {
+        return value1 / value2;
+    } else {
+        throw new Error('Operação inválida');
+    }
+}
+
+
+console.log('O resultado é', doOperation(operator, value1, valor2)) 
