ddbfe6967dcf4f792a8d9782fb040bd7d323ce6a
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-09-22 19:09:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
-// Requiring module
-const express = require('express');
- 
-// Creating express object
-const app = express();
- 
-// Handling GET request
-app.get('/', (req, res) => {
-    res.send('A simple Node App is '
-        + 'running on this server')
-    res.end()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.get ('/getRequest/:name', function (req,res){
+  res.send('Hello ' + req.params.name)
 })
- 
-// Port Number
-const PORT = process.env.PORT ||5000;
- 
-// Server Setup
-app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+
+app.post('/postRequest', function (req,res){
+  res.send('Hello, o post request deu certo!')
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
